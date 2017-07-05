f3f3157e00c4153b19210ce61a11ced3db6eff4d
“Alice <aliceletourneur@icloud.com>
2017-07-05 23:10:34 +0100

    Add incrementCounter functions to controller Create the init and render function for catView

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ var controller = {
     model.currentCat = model.cat[0];
     //Initialise both views
     catListView.init();
+    catView.init();
   },
   // Get currentCat data
   getCurrentCat: function() {
@@ -39,16 +40,23 @@ var controller = {
   // Set the currently selected cat as to the object passed in
   setCurrentCat: function(cat) {
     model.currentCat = cat;
+  },
+  // Add 1 to the click counter of selected cat
+  incrementCounter: function() {
+    model.currentCat.click++;
+    catView.render();
   }
 };
 
 /* ========= Views =========== */
 
 var catListView = {
+  // Renders the initial view of the list
   init:function() {
     this.catList = document.getElementById('catList');
     this.render();
   },
+  // Renders the list of cats
   render:function() {
     var cats = controller.getCats();
     this.catList.innerHTML = '';
@@ -56,9 +64,11 @@ var catListView = {
       var cat = cats[i];
       var listElem = document.createElement('li');
       listElem.textContent = cat.name;
+      // Renders selected cat in catView
       listElem.addEventListener('click',(function(cat) {
         return function(){
           controller.setCurrentCat(cat);
+          catView.render();
         }
       })(cat));
       catList.appendChild(listElem);
@@ -67,7 +77,27 @@ var catListView = {
 };
 
 var catView = {
+  //Renders the initial cat view
+  init: function() {
+    this.cat = document.getElementById('selectedCat');
+    this.catName = document.getElementById('catName');
+    this.catImage = document.getElementById('catImage');
+    this.catCount = document.getElementById('catCount');
+    this.catImage.setAttribute("width", "500");
+    // Calls the function to increment the click counter everytime user clicks the image of the cat
+    this.catImage.addEventListener('click',function() {
+      controller.incrementCounter();
+    })
+    this.render();
+  },
 
+  // Renders the cat view
+  render: function() {
+    var currentCat = controller.getCurrentCat();
+    this.catName.textContent = currentCat.name;
+    this.catCount.textContent = currentCat.click;
+    this.catImage.src = currentCat.picture;
+  }
 };
 
 
