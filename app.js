ec748385acb75bcf57c277b2deca5abd4cd1fc6c
“Alice <aliceletourneur@icloud.com>
2017-07-05 22:27:36 +0100

    Add controller's init and getCurrentCat functions Split View into 2 parts : catView and catListView Initiate the app with controller.init()

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 /* ========= Model =========== */
 
 var model = {
+  // currentCat holds the data of the cat that is selected by the user
   currentCat: null,
+  // Add array of cats
   cat: [
     {
     "name": "Charlie",
@@ -19,9 +21,27 @@ var model = {
 /* ========= Controller =========== */
 
 var controller = {
+  // Initialise controller
+  init: function() {
+    //Set the first cat of the list to be displayed when loading the app
+    model.currentCat = model.cat[0];
+  },
+  // Get currentCat data
+  getCurrentCat: function() {
+    return model.currentCat;
+  }
 };
 
-/* ========= View =========== */
+/* ========= Views =========== */
 
-var view = {
+var catView = {
+
 };
+
+var catListView = {
+
+};
+
+
+/* ========= Initiate the app =========== */
+controller.init();
