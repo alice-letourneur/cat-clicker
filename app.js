8e0649fcad89784cd1741d5b1107941c20c0bfa7
“Alice <aliceletourneur@icloud.com>
2017-07-05 21:31:59 +0100

    Create a model object, a controller object and a view object. Add array of cats in my model object

---
+++ app.js
@@ -0,0 +1,27 @@
+/* ========= Model =========== */
+
+var model = {
+  currentCat: null,
+  cat: [
+    {
+    "name": "Charlie",
+    "picture": "firstCat.jpg",
+    "click": 0
+    },
+    {
+      "name": "Roulette",
+      "picture": "secondCat.jpg",
+      "click": 0
+    }
+  ]
+};
+
+/* ========= Controller =========== */
+
+var controller = {
+};
+
+/* ========= View =========== */
+
+var view = {
+};
