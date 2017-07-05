131e8af0d1acdf5ae6a0d44c336bc56db60bd91c
“Alice <aliceletourneur@icloud.com>
2017-07-05 22:58:22 +0100

    Add pictures of cats Add getCats and setCurrentCat functions to controller Create the init and render function for catListView

--- app.js
+++ app.js
@@ -25,22 +25,52 @@ var controller = {
   init: function() {
     //Set the first cat of the list to be displayed when loading the app
     model.currentCat = model.cat[0];
+    //Initialise both views
+    catListView.init();
   },
   // Get currentCat data
   getCurrentCat: function() {
     return model.currentCat;
+  },
+  // Get all cats data
+  getCats: function() {
+    return model.cat;
+  },
+  // Set the currently selected cat as to the object passed in
+  setCurrentCat: function(cat) {
+    model.currentCat = cat;
   }
 };
 
 /* ========= Views =========== */
 
+var catListView = {
+  init:function() {
+    this.catList = document.getElementById('catList');
+    this.render();
+  },
+  render:function() {
+    var cats = controller.getCats();
+    this.catList.innerHTML = '';
+    for (var i = 0 ; i < cats.length; i++) {
+      var cat = cats[i];
+      var listElem = document.createElement('li');
+      listElem.textContent = cat.name;
+      listElem.addEventListener('click',(function(cat) {
+        return function(){
+          controller.setCurrentCat(cat);
+        }
+      })(cat));
+      catList.appendChild(listElem);
+  }
+ }
+};
+
 var catView = {
 
 };
 
-var catListView = {
 
-};
 
 
 /* ========= Initiate the app =========== */
